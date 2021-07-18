cc6a4ee09882236943a9a6b35f0f701e5adf2666
cathatino <cathatino@gmail.com>
2021-07-18 14:54:27 +0800

    Create userModel & a createUser func

--- grpc_service/internal/modules/user_profile/manager/dbmodel/user.go
+++ grpc_service/internal/modules/user_profile/manager/dbmodel/user.go
@@ -1,1 +1,19 @@
 package dbmodel
+
+// Design based on the suggestion of:
+// https://cloud.google.com/blog/products/identity-security/account-authentication-and-password-management-best-practices
+// https://stackoverflow.com/questions/11938044/what-are-the-best-practices-for-using-a-guid-as-a-primary-key-specifically-rega/11938495
+// https://www.sqlskills.com/blogs/kimberly/guids-as-primary-keys-andor-the-clustering-key/
+
+// UserModel defines the user account content,
+// while the login method will be handle in other tables to facilidate the different login mechanism
+// Note: let's start with a non-sharded version of the user table, assuming the user is with small size
+type UserModel struct {
+	UserId      int64  `xorm:"userid autoincr"`
+	Status      int8   `xorm:"status"`
+	AuthMethods string `xorm:"auth_methods"`
+	Profile     string `xorm:"profile"`
+	MetaData    string `xorm:"meta_data"`
+	Ctime       int64  `xorm:"ctime"`
+	Mtime       int64  `xorm:"mtime"`
+}
---
+++ grpc_service/internal/modules/user_profile/manager/user_manager.go
@@ -0,0 +1,51 @@
+package manager
+
+import (
+	"time"
+
+	"github.com/cathatino/topic-tree/grpc_service/internal/modules/user_profile/manager/dbmodel"
+	"github.com/go-xorm/xorm"
+)
+
+// UserManager provides the user related CRUD functionality
+type UserManager struct {
+	masterEngine *xorm.Engine
+	slaveEngine  *xorm.Engine
+}
+
+// CreateUser creates user based on the inputs
+// TODO: write test cases
+func (um UserManager) CreateUser(
+	authMethods string,
+	profile string,
+	metaData string,
+) (
+	*dbmodel.UserModel,
+	error,
+) {
+	currentTimeStampInSecs := time.Now().Unix()
+	userModel := &dbmodel.UserModel{
+		AuthMethods: authMethods,
+		Profile:     profile,
+		MetaData:    metaData,
+		Ctime:       currentTimeStampInSecs,
+		Mtime:       currentTimeStampInSecs,
+	}
+	affected, err := um.masterEngine.Insert(&userModel)
+	if err != nil {
+		return nil, err
+	}
+	userModel.UserId = affected
+	return userModel, err
+}
+
+// NewUserManager returns a new UserManager to manipulate the user data
+func NewUserManager(
+	masterEngine *xorm.Engine,
+	slaveEngine *xorm.Engine,
+) UserManager {
+	return UserManager{
+		masterEngine: masterEngine,
+		slaveEngine:  slaveEngine,
+	}
+}
