4a61dfa2140fea3ac623b311c50b97b119c8b980
cathatino <cathatino@gmail.com>
2021-07-22 22:26:45 +0800

    Add grpc service

--- grpc_service/cmd/main.go
+++ grpc_service/cmd/main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"fmt"
 
+	"context"
+	"log"
+	"net"
+
 	"github.com/cathatino/topic-tree/grpc_service/cnf"
 	"github.com/cathatino/topic-tree/grpc_service/infra/mysql"
 	"github.com/cathatino/topic-tree/grpc_service/internal/modules/user_profile/manager"
+
+	serverPb "github.com/cathatino/topic-tree/grpc_service/pb/server"
+	"google.golang.org/grpc"
 )
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	serverPb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *serverPb.HelloRequest) (*serverPb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &serverPb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
 
 func main() {
 	config := cnf.LoadConfig("../cnf/test.toml")
@@ -18,4 +40,15 @@ func main() {
 		"meta_data",
 	)
 	fmt.Println(userModel, err)
+	//
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	serverPb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- grpc_service/go.mod
+++ grpc_service/go.mod
@@ -6,5 +6,12 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-xorm/xorm v0.7.9
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
+	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/genproto v0.0.0-20210716133855-ce7ef5c701ea // indirect
+	google.golang.org/grpc v1.39.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.27.1
 )
